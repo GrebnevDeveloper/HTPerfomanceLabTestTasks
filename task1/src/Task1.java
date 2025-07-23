df694cab2ee7b0210412c01521f052194d96ad68
grebnev <grebnev.developer@gmail.com>
2025-07-23 20:34:19 +0400

    Task1 fix

--- task1/src/Task1.java
+++ task1/src/Task1.java
@@ -3,14 +3,17 @@ import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Введите размер массива и интервал длины через пробел: ");
-            int sizeArray = scanner.nextInt();
-            int lengthInterval = scanner.nextInt();
+        if (args.length < 2) {
+            System.err.println("Передано неверное количество аргументов");
+            return;
+        }
+        try {
+            int sizeArray = Integer.parseInt(args[0]);
+            int lengthInterval = Integer.parseInt(args[1]);
 
             findPath(sizeArray, lengthInterval);
 
-        } catch (InputMismatchException e) {
+        } catch (NumberFormatException e) {
             System.err.println("Ошибка: Введены нечисловые данные!");
         } catch (Exception e) {
             System.err.println("Неизвестная ошибка: " + e.getMessage());
@@ -20,28 +23,14 @@ public class Task1 {
     private static void findPath(int sizeArray, int lengthInterval) {
         StringBuilder path = new StringBuilder();
         int currentStart = 1;
-        int step = 1;
-
-        System.out.println("\nИнтервалы:");
 
         do {
-            StringBuilder interval = new StringBuilder();
-            System.out.print("Интервал " + step + ": ");
-
-            for (int i = 0; i < lengthInterval; i++) {
-                int position = ((currentStart + i - 1) % sizeArray) + 1;
-                interval.append(position);
-            }
-
-            System.out.println(interval);
-
             path.append(currentStart);
 
             currentStart = ((currentStart + lengthInterval - 2) % sizeArray) + 1;
-            step++;
 
         } while (currentStart != 1);
 
-        System.out.println("Полученный путь: " + path);
+        System.out.println(path);
     }
 }
