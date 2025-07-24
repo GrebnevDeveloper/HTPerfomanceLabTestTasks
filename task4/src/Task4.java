63cff5a80e0be04e8df97c927c791a8b3067c4d2
grebnev <grebnev.developer@gmail.com>
2025-07-24 05:27:11 +0400

    Task4 complete

---
+++ task4/src/Task4.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Task4 {
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.err.println("Неверное количество аргументов");
+            return;
+        }
+
+        try {
+            String content = Files.readString(Paths.get(args[0]));
+
+            List<Integer> nums = Arrays.stream(content.split("\\n"))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());;
+
+            System.out.println(minMovesToEqualElements(nums));
+
+        } catch (IOException e) {
+            System.err.println("Ошибка при чтении файла: " + e.getMessage());
+        } catch (NumberFormatException e) {
+            System.err.println("Ошибка при парсинге чисел: " + e.getMessage());
+        }
+    }
+
+    private static int minMovesToEqualElements(List<Integer> nums) {
+        Collections.sort(nums);
+        int median = nums.get(nums.size() / 2);
+        int moves = 0;
+        for (int num : nums) {
+            moves += Math.abs(num - median);
+        }
+        return moves;
+    }
+}
